6c1ebbd8ab8488327ae0517a41a9afb16ec56ba8
Roman Edelev <roman.edelev.dev@gmail.com>
2026-07-26 01:26:49 +0300

    тест коммита. начало v0.4

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
         System.out.println("Игра: КреСТИкИ-НоЛИки");
         System.out.println();
 
+        System.out.println("Играть с ботом или нет? (y/n)");
+
         // Здесь мы видим "new Scanner.." -> ожидаем тип Scanner. Поэтому можем не писать "Scanner (имя)", а пишем "var (имя)", оно короче и понятней
         var scanner = new Scanner(System.in); // System.in означает, что мы ожидаем ввод с клавиатуры. Также можно вместо этого вписать файл и читать его
         int xWins = 0;
@@ -183,4 +185,86 @@ public class Main {
             }
         }
     }
+    // Для игры с ботом
+    private static char playOneGameWithBot(Scanner scanner) { // Извне берется только сканер
+        // Поле
+        char[][] board = {
+                {'.', '.', '.'},
+                {'.', '.', '.'},
+                {'.', '.', '.'}
+        };
+
+        // Текущий ход. Начало с "х"
+        char turn = 'x';
+
+        // Счетчик ходов
+        int moveCount = 0;
+
+        while (true) {
+            System.out.println();
+            printBoard(board); // Вывод поля
+            System.out.println();
+
+            System.out.println("Сейчас ходит: " + turn);
+
+            // Запрос у пользователя ячейки поля
+            // Блок try/catch сохраняет переменные инициализированные внутри него оставляем внутри себя
+            // Поэтому объявляем заранее
+            int row = 0;
+            int col = 0;
+
+            try {
+                System.out.print("Введите номер строки: (0-2)");
+                row = scanner.nextInt(); // Здесь нельзя писать "var row", читаемость упадет. Всегда, где цифры лучше не исп. "var"
+                if (row < 0 || row > 2) {
+                    throw new IllegalArgumentException();
+                }
+
+                System.out.print("Введите номер столбца: (0-2)");
+                col = scanner.nextInt();
+                if (col < 0 || col > 2) {
+                    throw new IllegalArgumentException(); // Если индекса такой ячейки нет (выход за пределы массива) - вызываем ошибку
+                }
+
+            } catch (InputMismatchException |
+                     IllegalArgumentException e) { // InputMismatchException - ошибка, получил тип данных не который ожидал
+                // Т.к. ранее мы выбросили ошибку с аргументом, то мы должны ее ловить в catch, а также нужно поймать ошибку с типами данных
+                // Для этого используется multi-catch - это |.
+                // e - нужен, чтобы хранить внутри себя данные об ошибке, потом можно будет вывести ее и посмотреть + это обязательный синтаксис Java
+                // Важно! Из-за multi-catch 2 разные ошибки сохраняются в 1 e
+                System.out.println("Ошибка ввода! Введите число от 0 до 2 (включительно)");
+                // Важно! Если user введет например "Пока", а контейнер ожидает Int, он заберет "Пока" и сохранит его и когда после
+                // обработки ошибки он вернется к этому этапу он уже будет содержать "Пока" и опять будет ошибка. Поэтому Важно его очистить.
+                scanner.nextLine(); // Очистка контейнера
+                continue;
+            }
+
+            // Проверка на занятость ячейки
+
+            if (board[row][col] == '.') {
+                moveCount++; // Увеличиваем ход на 1
+                board[row][col] = turn; // Заполняем ячейку
+
+                if (isWin(turn, board)) {
+                    printBoard(board); // Печать поля для красоты
+                    System.out.println();
+                    System.out.println("Победил игрок: " + turn + ". Поздравляем!");
+                    return turn;
+                }
+
+                // Проверка на ничью
+                if (isDraw(moveCount)) {
+                    printBoard(board); // Печать поля для красоты
+                    System.out.println();
+                    System.out.println("Ничья!");
+                    return 'd';
+                }
+
+                // Смена хода
+                turn = nextTurn(turn);
+
+            } else {
+                System.out.println("Поле уже занято, выберите другое");
+            }
+        }
 }
